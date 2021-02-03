e8f472248782319e4bb746b3cb4bd7b8849b557f
Martin Švec <martin.svec@slovenskoit.sk>
2021-02-03 21:49:40 +0100

    Revert "bootstrap update to v.4.3.1"
    This reverts commit 62792e4136dabe357fab8f09defb13388f023bf6.

--- www/includes/web_functions.inc.php
+++ www/includes/web_functions.inc.php
@@ -221,9 +221,9 @@ function render_header($title="",$menu=TRUE) {
  <TITLE><?php print "$title"; ?></TITLE>
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+ <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </HEAD>
 <BODY>
 <?php
