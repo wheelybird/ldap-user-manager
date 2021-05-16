73d7aba2a9363095bbbe0bc07902da752d198a72
Thatoo <Thatoo@users.noreply.github.com>
2021-05-16 15:05:31 +0200

    Update config.inc.php

--- www/includes/config.inc.php
+++ www/includes/config.inc.php
@@ -45,7 +45,7 @@
  $ORGANISATION_NAME = (getenv('ORGANISATION_NAME') ? getenv('ORGANISATION_NAME') : 'LDAP');
  $SITE_NAME = (getenv('SITE_NAME') ? getenv('SITE_NAME') : "$ORGANISATION_NAME user manager");
  $SERVER_HOSTNAME = (getenv('SERVER_HOSTNAME') ? getenv('SERVER_HOSTNAME') : "ldapusermanager.org");
- $SUBFOLDER = (getenv('HTTP_SUBFOLDER') ? getenv('HTTP_SUBFOLDER') : "");
+ if (getenv('HTTP_SUBFOLDER')) { $SUBFOLDER = getenv('HTTP_SUBFOLDER') . "/"; }
 
 $ENFORCE_SAFE_SYSTEM_NAMES = ((strcasecmp(getenv('ENFORCE_SAFE_SYSTEM_NAMES'),'FALSE') == 0) ? FALSE : TRUE);
  $POSIX_USERNAME_FORMAT = (getenv('USERNAME_FORMAT') ? getenv('USERNAME_FORMAT') : '{first_name}-{last_name}');
