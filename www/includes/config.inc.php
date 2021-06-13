6db55477aa4a8b7da2bcb002058d3f674eef9e7a
Damian Galli <damian.galli@galli.site>
2021-06-13 10:43:36 +0200

    Implement Remote Headers Auth

--- www/includes/config.inc.php
+++ www/includes/config.inc.php
@@ -109,6 +109,12 @@
 
  ###
 
+ ###
+
+ $REMOTE_HTTP_HEADERS_LOGIN = ((strcasecmp(getenv('REMOTE_HTTP_HEADERS_LOGIN'),'TRUE') == 0) ? TRUE : FALSE);
+ 
+ ###
+
  $errors = "";
 
  if (empty($LDAP['uri'])) {
